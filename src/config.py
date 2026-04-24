6065e362145f0accc753b0fd0478e191178e2b0c
nzenith04 <nzenith08@gmail.com>
2026-04-24 12:19:35 +0530

    udts

--- src/config.py
+++ src/config.py
@@ -6,3 +6,6 @@ DATA_PATH = os.path.join(BASE_DIR, "data/processed/recipes_cleaned.json")
 
 FAISS_INDEX_PATH = os.path.join(BASE_DIR, "faiss_index/recipes_ivf.index")
 FAISS_META_PATH = os.path.join(BASE_DIR, "faiss_index/metadata.json")
+HF_CACHE_DIR = os.path.join(BASE_DIR, "model_cache")
+
+os.makedirs(HF_CACHE_DIR, exist_ok=True)
